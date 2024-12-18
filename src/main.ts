3e18f891904a2b4d8415a9ea111da617257a379f
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2024-12-18 23:54:38 +0800

    build (package): installed vuex and IntlTelInput
    Installed vuex and IntlTelInput

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,29 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
+import { createStore } from 'vuex'
 import App from './App.vue'
 import router from './router'
 
+const store = createStore({
+  state() {
+    return {
+      profile: {
+        name: 'John Doe',
+        email: 'jane@gmail.com',
+        phoneNum: '+01 234 567 89',
+      },
+    }
+  },
+  mutations: {
+    updateProfile(state, key: string, value: string) {
+      state.profile[key] = value
+    },
+  },
+})
+
 const app = createApp(App)
 
-app.use(router)
+app.use(router, store)
 
 app.mount('#app')
