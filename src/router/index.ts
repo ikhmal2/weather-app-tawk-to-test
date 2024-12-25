c85ae45f3e8bb98a090f67a8a2b58f5dde6bea0c
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2024-12-26 01:50:49 +0800

    feat: initial work on weather details page

--- src/router/index.ts
+++ src/router/index.ts
@@ -12,6 +12,7 @@ const router = createRouter({
     {
       path: '/weather',
       name: 'weather',
+      props: true,
       // route level code-splitting
       // this generates a separate chunk (About.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
--- src/views/WeatherDetails.vue
+++ src/views/WeatherDetails.vue
@@ -1,9 +1,55 @@
 <template>
-	<div>
-		<h1>Hello world</h1>
+	<div class="highlight">
+		<RouterLink to="/">
+			<img src="../assets/img/left-white.svg" alt="Left" />
+		</RouterLink>
+		<div class="name">{{ name }}</div>
+		<img src="../assets/img/add.svg" alt="Add">
 	</div>
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted } from 'vue';
+import { useRoute } from 'vue-router';
+const route = useRoute();
 
-<style scoped></style>
+const lat = route.query.lat
+const long = route.query.long
+const name = route.query.name
+console.log(name);
+
+async function getWeatherData() {
+	const url = `https://api.openweathermap.org/data/2.5/forecast?lat=${lat}&lon=${long}&units=metric&appid=3e4ab56c9ba5a0c2807affcd2226e2a3`
+	try {
+		if (lat !== null || long !== null) {
+			const resp = await fetch(url)
+			if (!resp.ok) {
+				throw new Error(`Response status: ${resp.status}`)
+			}
+			const response = await resp.json()
+			console.log(response);
+		} else {
+			throw new Error("Can't retrieve Long Lang")
+		}
+	} catch (error) {
+		console.log(error)
+	}
+}
+
+onMounted(() => {
+	// getWeatherData()
+})
+</script>
+
+<style scoped>
+.highlight {
+	background: linear-gradient(118.25deg, #4F80FA 1.2%, #3764D7 59.26%, #335FD1 79.2%);
+	color: white;
+	display: flex;
+	justify-content: space-between;
+}
+
+.name {
+	font-size: .875rem;
+}
+</style>
