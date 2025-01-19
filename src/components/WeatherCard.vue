33f33c4cfac321475077c883b0ff612bc7719435
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2025-01-20 01:39:15 +0800

    feat: update card to get correct hi lo values and added day/night class

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,61 +1,55 @@
 <template>
-  <div v-if="Object.keys(weatherData).length !== 0" class="weather-card">
+  <div v-if="weatherData" class="weather-card night">
     <div class="location">
       <div class="location-details">
         <p class="location-name">My Location</p>
         <p class="location-subname">{{ locationName }}</p>
       </div>
       <div class="location-weather">{{ weatherData.current.weather[0].description }}</div>
-      <div class="location-weather">
-        {{
-          new Date(weatherData.currentTime).toLocaleTimeString(
-            "en-us",
-            {
-              timeStyle: "short",
-            }
-          )
-        }}</div>
     </div>
     <div class="temperature">
       <div class="current-temperature">{{ Math.round(weatherData.current.temp) }} °</div>
-      <div class="hi-low">H: {{ todayWeather.hi }}° L:{{ todayWeather.low }}°</div>
+      <div class="hi-low">
+        H: {{ Math.round(weatherData.daily[0].temp.max) }}° L:{{
+          Math.round(weatherData.daily[0].temp.min)
+        }}°
+      </div>
     </div>
   </div>
 </template>
 
 <script setup lang="ts">
 import axios from 'axios'
-import { onMounted, ref, computed } from 'vue'
+import { onMounted, ref } from 'vue'
+import { Openweather } from '@/types'
 
-const position = ref<GeolocationPosition>();
+const position = ref<GeolocationPosition>()
 
 const errMsg = ref('')
-const weatherData = ref({})
-const locationName = ref('');
+const weatherData = ref<Openweather>()
+const locationName = ref('')
 
 async function getWeatherData(pos: GeolocationPosition) {
   const lat = await pos.coords.latitude
   const long = await pos.coords.longitude
   const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=metric&appid=${import.meta.env.VITE_WEATHEROPENAPI__KEY}`
   try {
-    const resp = await axios.get(url);
+    const resp = await axios.get(url)
 
     // cal current date & time
-    const localOffset = new Date().getTimezoneOffset() * 60000;
-    const utc = resp.data.current.dt * 1000 + localOffset;
-    resp.data.currentTime =
-      utc + 1000 * resp.data.timezone_offset;
+    const localOffset = new Date().getTimezoneOffset() * 60000
+    const utc = resp.data.current.dt * 1000 + localOffset
+    resp.data.currentTime = utc + 1000 * resp.data.timezone_offset
 
     // cal hourly weather offset
     resp.data.hourly.forEach((hour) => {
-      const utc = hour.dt * 1000 + localOffset;
-      hour.currentTime =
-        utc + 1000 * resp.data.timezone_offset;
-    });
+      const utc = hour.dt * 1000 + localOffset
+      hour.currentTime = utc + 1000 * resp.data.timezone_offset
+    })
     getLocationName(lat, long)
-    weatherData.value = resp.data;
+    weatherData.value = resp.data
   } catch (err) {
-    console.log(err);
+    console.log(err)
   }
 }
 
@@ -69,44 +63,18 @@ function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (pos) => {
-        position.value = pos;
+        position.value = pos
         getWeatherData(pos)
       },
       (error) => {
-        if (position.value) errMsg.value = `Error getting location: ${error.message}`;
-      }
-    );
+        if (position.value) errMsg.value = `Error getting location: ${error.message}`
+      },
+    )
   } else {
-    if (position.value) errMsg.value = 'Geolocation is not supported by this browser.';
+    if (position.value) errMsg.value = 'Geolocation is not supported by this browser.'
   }
 }
 
-const todayWeather = computed(() => {
-  const weatherDetails = {
-    temp: 0,
-    hi: 0,
-    low: 0,
-    weather: '',
-  }
-
-  if (!weatherData.value?.list?.length) return weatherDetails
-
-  const today = new Date().toISOString().slice(0, 10)
-
-  const todayData = weatherData.value.list.find((item) => {
-    const itemDate = item.dt_txt
-    return itemDate.includes(today)
-  })
-
-  if (todayData) {
-    weatherDetails.temp = Math.round(todayData.main.temp)
-    weatherDetails.hi = Math.round(todayData.main.temp_max)
-    weatherDetails.low = Math.round(todayData.main.temp_min)
-    weatherDetails.weather = todayData.weather[0]?.description || ''
-  }
-
-  return weatherDetails
-})
 onMounted(() => {
   getLocation()
 })
@@ -117,11 +85,17 @@ onMounted(() => {
   width: 100%;
   display: flex;
   justify-content: space-between;
-  border: 1px dotted red;
-  background-color: black;
   color: white;
   padding: 0.5rem;
   border-radius: 16px;
+}
+
+.night {
+  background-image: url(../assets/img/night.png);
+}
+
+.day {
+  background-image: url(../assets/img/day.png);
 }
 
 .location {
@@ -153,12 +127,12 @@ onMounted(() => {
   justify-content: space-between;
 }
 
-.temperature>.current-temperature {
+.temperature > .current-temperature {
   font-size: 3.3125rem;
   line-height: 63.25px;
 }
 
-.temperature>.hi-low {
+.temperature > .hi-low {
   font-size: 0.9375rem;
   line-height: 20px;
 }
