65634676c5b80a4323ca31f6533c787677eda975
Ikhmal Fahmi <ax.ikhmal.145713@gmail.com>
2025-01-26 22:51:37 +0800

    feat: added loading screen

--- src/components/WeatherCard.vue
+++ src/components/WeatherCard.vue
@@ -1,6 +1,10 @@
 <template>
-  <div @click="goToWeatherDetails" v-if="weatherData && weather && weather.current && store.showCurrent"
-    class="weather-card" :class="[dayTime ? 'day' : 'night']">
+  <div
+    @click="goToWeatherDetails"
+    v-if="weatherData && weather && weather.current && store.showCurrent"
+    class="weather-card"
+    :class="[dayTime ? 'day' : 'night']"
+  >
     <div class="location">
       <div class="location-details">
         <p class="location-name">My Location</p>
@@ -17,16 +21,22 @@
       </div>
     </div>
   </div>
-  <div @click="goToWeatherDetails" v-if="weather && weatherData && !weather.current" class="weather-card"
-    :class="[dayTime ? 'day' : 'night']">
-    <div class=" location">
+  <div
+    @click="goToWeatherDetails"
+    v-if="weather && weatherData && !weather.current"
+    class="weather-card"
+    :class="[dayTime ? 'day' : 'night']"
+  >
+    <div class="location">
       <div class="location-details">
         <p class="location-subname">{{ locationName }}</p>
-        <p class="time">{{
-          new Date(weatherData.currentTime).toLocaleTimeString('en-us', {
-            timeStyle: 'short',
-          })
-        }}</p>
+        <p class="time">
+          {{
+            new Date(weatherData.currentTime).toLocaleTimeString('en-us', {
+              timeStyle: 'short',
+            })
+          }}
+        </p>
       </div>
       <div class="location-weather">{{ weatherData.current.weather[0].description }}</div>
     </div>
@@ -42,93 +52,95 @@
 </template>
 
 <script setup lang="ts">
-import axios from 'axios'
-import { computed, onMounted, ref } from 'vue'
-import { Current, Openweather } from '@/types'
-import { useWeather, storedLocations } from '@/store'
-import router from '@/router'
+import axios from 'axios';
+import { computed, onMounted, ref } from 'vue';
+import { Current, Openweather } from '@/types';
+import { useWeather, useLoadingState, storedLocations } from '@/store';
+import router from '@/router';
 
 const props = defineProps({
   weather: {
     type: [Object],
-    required: false
-  }
-})
+    required: false,
+  },
+});
 
-const position = ref<GeolocationPosition>()
-const store = useWeather()
+const position = ref<GeolocationPosition>();
+const store = useWeather();
+const loadingStore = useLoadingState();
 
-const errMsg = ref('')
-const weatherData = ref<Openweather>()
-const locationName = ref('')
+const errMsg = ref('');
+const weatherData = ref<Openweather>();
+const locationName = ref('');
 
 async function getWeatherData(lat: number, long: number) {
-  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=metric&appid=${import.meta.env.VITE_WEATHEROPENAPI__KEY}`
+  const url = `https://api.openweathermap.org/data/3.0/onecall?lat=${lat}&lon=${long}&units=metric&&exclude=minutely,&appid=${import.meta.env.VITE_WEATHEROPENAPI__KEY}`;
   try {
-    const resp = await axios.get(url)
+    loadingStore.updateLoadingState(true);
+    const resp = await axios.get(url);
+    loadingStore.updateLoadingState(false);
 
     // cal current date & time
-    const localOffset = new Date().getTimezoneOffset() * 60000
-    const utc = resp.data.current.dt * 1000 + localOffset
-    resp.data.currentTime = utc + 1000 * resp.data.timezone_offset
+    const localOffset = new Date().getTimezoneOffset() * 60000;
+    const utc = resp.data.current.dt * 1000 + localOffset;
+    resp.data.currentTime = utc + 1000 * resp.data.timezone_offset;
 
     // cal hourly weather offset
     resp.data.hourly.forEach((hour: Current) => {
-      const utc = hour.dt * 1000 + localOffset
-      hour.currentTime = utc + 1000 * resp.data.timezone_offset
-    })
-    getLocationName(lat, long)
-    weatherData.value = resp.data
+      const utc = hour.dt * 1000 + localOffset;
+      hour.currentTime = utc + 1000 * resp.data.timezone_offset;
+    });
+    getLocationName(lat, long);
+    weatherData.value = resp.data;
     if (!props.weather) {
       const currentLocation: storedLocations = {
         longitude: long,
         latitude: lat,
         name: locationName.value,
-      }
-      store.addList(currentLocation)
+      };
+      store.addList(currentLocation);
     }
   } catch (err) {
-    console.log(err)
+    console.log(err);
   }
 }
 
 async function getLocationName(lat: number, long: number) {
-  const url = `https://api.mapbox.com/search/geocode/v6/reverse?longitude=${long}&latitude=${lat}&limit=1&types=place&access_token=${import.meta.env.VITE_MAPBOX_API_KEY}`
-  const resp = await axios.get(url)
-  locationName.value = resp.data.features[0].properties.name
+  const url = `https://api.mapbox.com/search/geocode/v6/reverse?longitude=${long}&latitude=${lat}&limit=1&types=place&access_token=${import.meta.env.VITE_MAPBOX_API_KEY}`;
+  const resp = await axios.get(url);
+  locationName.value = resp.data.features[0].properties.name;
 }
 
 function getLocation() {
   if (navigator.geolocation) {
     navigator.geolocation.getCurrentPosition(
       (pos) => {
-        position.value = pos
-        getWeatherData(pos.coords.latitude, pos.coords.longitude)
+        position.value = pos;
+        getWeatherData(pos.coords.latitude, pos.coords.longitude);
       },
       (error) => {
-        if (position.value) errMsg.value = `Error getting location: ${error.message}`
+        if (position.value) errMsg.value = `Error getting location: ${error.message}`;
       },
-    )
+    );
   } else {
-    if (position.value) errMsg.value = 'Geolocation is not supported by this browser.'
+    if (position.value) errMsg.value = 'Geolocation is not supported by this browser.';
   }
 }
 
 const dayTime = computed(() => {
-  let currTime
+  let currTime;
   if (weatherData.value) {
-    currTime = new Date(weatherData.value.currentTime)
+    currTime = new Date(weatherData.value.currentTime);
   } else if (props.weather && !weatherData.value) {
-    currTime = new Date(props.weather.data.currentTime)
+    currTime = new Date(props.weather.data.currentTime);
   }
 
-  if (!currTime) return true
+  if (!currTime) return true;
 
-  const hours = currTime.getHours()
+  const hours = currTime.getHours();
 
-  return hours >= 6 && hours < 18
-})
-
+  return hours >= 6 && hours < 18;
+});
 
 function goToWeatherDetails() {
   router.push({
@@ -137,18 +149,18 @@ function goToWeatherDetails() {
       long: props.weather?.longitude,
       lat: props.weather?.latitude,
       name: props.weather?.name,
-      current: props.weather?.current
+      current: props.weather?.current,
     },
-  })
+  });
 }
 
 onMounted(() => {
   if (!props.weather) {
-    getLocation()
+    getLocation();
   } else if (props.weather) {
-    getWeatherData(props.weather.latitude, props.weather.longitude)
+    getWeatherData(props.weather.latitude, props.weather.longitude);
   }
-})
+});
 </script>
 
 <style scoped>
@@ -177,8 +189,8 @@ onMounted(() => {
   display: flex;
   flex-direction: column;
   justify-content: space-between;
-  padding-left: .5rem;
-  padding-top: .2rem;
+  padding-left: 0.5rem;
+  padding-top: 0.2rem;
 }
 
 .location-name {
@@ -204,12 +216,12 @@ onMounted(() => {
   justify-content: space-between;
 }
 
-.temperature>.current-temperature {
+.temperature > .current-temperature {
   font-size: 3.3125rem;
   line-height: 63.25px;
 }
 
-.temperature>.hi-low {
+.temperature > .hi-low {
   font-size: 0.9375rem;
   line-height: 20px;
 }
